4ade852289612fadcfeed484f7e5663127e51466
celestn1 <contactilec@gmail.com>
2025-06-08 20:21:35 +0100

    Trigger: Test deploy pipeline

--- terraform/modules/eks/outputs.tf
+++ terraform/modules/eks/outputs.tf
@@ -9,8 +9,3 @@ output "cluster_endpoint" {
   value       = module.eks.cluster_endpoint
   description = "EKS API endpoint"
 }
-
-output "kubeconfig" {
-  value       = module.eks.kubeconfig
-  description = "Kubeconfig content for the EKS cluster"
-}
