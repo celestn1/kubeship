1290faebdba92071d2ea80c625495ece9fbb0828
celestn1 <contactilec@gmail.com>
2025-06-10 00:29:33 +0100

    [skip ci]: Updated the destroy workflow

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -2,7 +2,7 @@
 
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "20.36.0"
+  version = "~> 20.36.0"
 
   cluster_name    = var.cluster_name
   cluster_version = var.cluster_version
