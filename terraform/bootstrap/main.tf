7870479291cd11dc730c67044118a090ac4a5b72
celestn1 <contactilec@gmail.com>
2025-06-09 01:07:28 +0100

    Trigger: Test AWS deploy pipeline - Updated Docker Images

--- terraform/bootstrap/main.tf
+++ terraform/bootstrap/main.tf
@@ -1,6 +1,6 @@
 // kubeship/terraform/bootstrap/main.tf
 
-# run once to set up the S3 bucket and DynamoDB table for Terraform State Managemement
+# run once to set up the S3 bucket and DynamoDB table for Terraform State Magemement
 provider "aws" {
   region = var.aws_region
 }
