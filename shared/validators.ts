759eeca7f9469e7309b0e34e3d497ea44f86495e
celestn1 <contactilec@gmail.com>
2025-06-21 00:02:38 +0100

    Initialize test deployment

--- shared/validators.ts
+++ shared/validators.ts
@@ -1,5 +1,5 @@
 // kubeship/shared/validators.ts
-// Added to test docker image push 7
+// Added to test docker image push 8
 export const USERNAME_REGEX = /^[a-zA-Z0-9_]{3,30}$/;
 
 export const isValidUsername = (username: string): boolean => {
