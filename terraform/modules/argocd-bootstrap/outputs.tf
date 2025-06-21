53dfa8110ad02346b05b67da474a163b9d1371f2
celestn1 <contactilec@gmail.com>
2025-06-21 16:45:07 +0100

    ArgoCD Application manifests

--- terraform/modules/argocd-bootstrap/outputs.tf
+++ terraform/modules/argocd-bootstrap/outputs.tf
@@ -4,10 +4,3 @@ output "argocd_namespace" {
   description = "Namespace where ArgoCD is installed"
   value       = kubernetes_namespace.argocd.metadata[0].name
 }
-
-output "argocd_server_lb_hostname" {
-  description = "LoadBalancer hostname of ArgoCD server"
-  value       = helm_release.argocd.status[0].load_balancer_ingress[0].hostname
-  // Note: This may be empty if LB is not ready immediately
-  // Alternatively use a kubernetes_service data source if needed
-}
