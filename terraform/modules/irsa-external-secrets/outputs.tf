04b49e7c60b77df32814f582a24f8dbf36a4a483
celestn1 <contactilec@gmail.com>
2025-06-21 18:45:04 +0100

    ArgoCD Application manifests

--- terraform/modules/irsa-external-secrets/outputs.tf
+++ terraform/modules/irsa-external-secrets/outputs.tf
@@ -1,6 +1,6 @@
 # kubeship/terraform/modules/irsa-external-secrets/outputs.tf
 
-#output "iam_role_arn" {
-#  description = "IAM Role ARN for External Secrets IRSA"
-#  value       = aws_iam_role.irsa_role.arn
-#}
+output "iam_role_arn" {
+  description = "IAM Role ARN for External Secrets IRSA"
+  value       = aws_iam_role.irsa_role.arn
+}
