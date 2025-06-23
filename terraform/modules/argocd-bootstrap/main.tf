709764bc73b1f3c3eefeb5ef364603971344da93
celestn1 <contactilec@gmail.com>
2025-06-23 13:21:38 +0100

    Trigger Full EKS test deploy

--- terraform/modules/argocd-bootstrap/main.tf
+++ terraform/modules/argocd-bootstrap/main.tf
@@ -54,9 +54,6 @@ resource "kubernetes_storage_class" "gp3" {
 
   metadata {
     name = "gp3"
-    annotations = {
-      "storageclass.kubernetes.io/is-default-class" = "true"
-    }
   }
 
   storage_provisioner = "ebs.csi.aws.com"
