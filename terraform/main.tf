7f7b803ccde5015388cf4e61bc20e482a46c14b3
celestn1 <contactilec@gmail.com>
2025-06-23 23:09:01 +0100

    Trigger test deploy - ALB Controller & NGINX ingress

--- terraform/main.tf
+++ terraform/main.tf
@@ -62,6 +62,8 @@ module "alb" {
   project_name      = var.project_name
   vpc_id            = module.vpc.vpc_id
   public_subnet_ids = module.vpc.public_subnet_ids
+  aws_region        = var.aws_region
+  cluster_name      = var.eks_cluster_name
 }
 
 # EKS cluster provisioning (registry module)
